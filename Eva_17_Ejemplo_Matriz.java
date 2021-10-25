7255dcfe1327e3abe9fe2e2791dc697827c33b72
Levi Caro <92340774+LeviCaro@users.noreply.github.com>
2021-10-24 21:09:31 -0600

    Add files via upload

---
+++ Eva_17_Ejemplo_Matriz.java
@@ -0,0 +1,24 @@
+/**
+ *
+ * @author Daniel Levi Caro Alvarado
+ */
+package Eva1_17_Ejemplo_Matriz;
+
+
+public class Eva_17_Ejemplo_Matriz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[][] aiMatriz = new int [3][];
+        //Filas
+        aiMatriz[0] = new int[2];//fila cero son dos columnas
+        aiMatriz[1] = new int[10];//fila uno son diez columnas
+        aiMatriz[2] = new int[5];//fila dos son dos culumnas
+        //Cuantos enteros puedo almacenar?
+        //tenemos 17 enteros 
+    }
+    
+}
