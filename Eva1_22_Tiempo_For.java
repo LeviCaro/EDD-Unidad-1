979e71dc7c26b7b7d9316b23c7da0982f4bf8f03
Levi Caro <92340774+LeviCaro@users.noreply.github.com>
2021-10-24 21:14:11 -0600

    Add files via upload

---
+++ Eva1_22_Tiempo_For.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_22_tiempo_for;
+
+/**
+ *
+ * @author Admin
+ */
+public class Eva1_22_Tiempo_For {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] aiArray = new int[100000000];//400 Megas
+        for (int i = 0; i < aiArray.length; i++) {
+            aiArray[i] = 100;
+        }
+        
+    }
+    
+}
