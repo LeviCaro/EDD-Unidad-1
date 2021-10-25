a015d4d21ff01e34bf993ce67bd7b1a19c601a2f
Levi Caro <92340774+LeviCaro@users.noreply.github.com>
2021-10-24 21:02:08 -0600

    Add files via upload

---
+++ Eva1_2_Stack.java
@@ -0,0 +1,36 @@
+/**
+ *
+ * @author Daniel Levi Caro Alvarado
+ */
+package eva1_2_stack;
+
+
+public class Eva1_2_Stack {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Inicia main");
+        A ();
+        System.out.println("Termina main");
+    }
+    
+    public static void A (){
+        System.out.println("Inicia A");
+        B();
+        System.out.println("Termina A");
+    }
+    
+    public static void B(){
+        System.out.println("Inicia B");
+        int i = 0;
+        double d = 0.0;
+        System.out.println("Termina B");
+    }
+    
+}
+    
+        
+    
