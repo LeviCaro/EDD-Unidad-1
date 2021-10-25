4a8eaa75575f23ecba7c397b962aa336e50ffc9c
Levi Caro <92340774+LeviCaro@users.noreply.github.com>
2021-10-24 21:14:29 -0600

    Add files via upload

---
+++ Eva1_23_Numeros_Primos.java
@@ -0,0 +1,26 @@
+/**
+ *
+ * @author Daniel Levi Caro Alvarado
+ */
+package eva1_23_numeros_primos;
+
+import java.util.Scanner;
+
+
+public class Eva1_23_Numeros_Primos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        System.out.println("Introduce el numero: ");
+        int iVal = input.nextInt();
+        for (int i = 2; i < iVal; i++) {
+            //Division: iVal / i
+            //Si la division es exacta
+        }
+    }
+    
+}
