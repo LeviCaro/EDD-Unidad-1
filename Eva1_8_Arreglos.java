729d179746eacbc6b5a70bec0b019c1383ef084b
Levi Caro <92340774+LeviCaro@users.noreply.github.com>
2021-10-24 21:05:17 -0600

    Add files via upload

---
+++ Eva1_8_Arreglos.java
@@ -0,0 +1,27 @@
+/**
+ *
+ * @author Daniel Levi Caro Alvarado
+ */
+package eva1_8_arreglos;
+
+
+public class Eva1_8_Arreglos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] aiMiArreglo;//Declaracion, rl arreglo no existe todavia
+        //Arreglos son objetos:
+        aiMiArreglo = new int[10];//Asignamient memoria para 10 enteros
+        
+        int[] aiOtroArreglo = new int [50];//declaramos y creamos el arrglo
+        
+        double[] adArre1, adArre2, adArre3;
+        
+        double adOtroArre[], dVariable, dOtraVar;//solo un arreglo, dos variables de tipo double
+        
+    }
+    
+}
