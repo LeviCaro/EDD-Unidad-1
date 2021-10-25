d13919f289cfe75fbba9bcc83aa2f623138f8dfe
Levi Caro <92340774+LeviCaro@users.noreply.github.com>
2021-10-24 21:15:19 -0600

    Update Eva1_22_Tiempo_For.java

--- Eva1_22_Tiempo_For.java
+++ Eva1_22_Tiempo_For.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ *
+ * @author Daniel Levi Caro Alvarado
  */
 package eva1_22_tiempo_for;
 
-/**
- *
- * @author Admin
- */
+
 public class Eva1_22_Tiempo_For {
 
     /**
